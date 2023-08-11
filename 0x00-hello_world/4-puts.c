efdf0e16bb7dc1f55f74809354604e72cc4b8efd
youssefelfadaly <youssefelfadaly2018@gmail.com>
2023-08-10 17:02:10 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main - entry point
+ *
+ * Describtion : a programme that prints using put
+ *
+ * return always 0 it means (success)
+ */
+
+
+
+int main(void)
+{
     puts("\"Programming is like building a multilingual puzzle");
     return(0);
 
