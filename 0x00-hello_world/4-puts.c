eb1c7578f7f374df7b6dabe07bd308acfba87910
youssefelfadaly <youssefelfadaly2018@gmail.com>
2023-08-10 17:08:23 -0700

    v

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,14 @@
  * Describtion : a programme that prints using put
  *
  * return always 0 it means (success)
+ * return value main equal 0
  */
 
 
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
 }
