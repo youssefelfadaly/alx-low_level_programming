d9b230f9a40d7bfda0ee5b134329b9bc31cc8437
youssefelfadaly <youssefelfadaly2018@gmail.com>
2023-08-10 17:36:08 -0700

    C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C programme that print text with printf
+ *
+ * Return: return always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
+
