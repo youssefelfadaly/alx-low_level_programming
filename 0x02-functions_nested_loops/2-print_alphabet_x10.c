bfc0ae94ec296463f52255fbaf0359d02d2e30ac
youssefelfadaly <youssefelfadaly2018@gmail.com>
2023-08-16 16:54:56 -0700

    u

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
 void print_alphabet_x10(void)
 {
 
-	char num, letter;
+char num, letter;
 
 for (num = 0; num <= 9; num++)
 
 {
 
-			for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 
 {
 
-				_putchar(letter);
+_putchar(letter);
 
 }
 
